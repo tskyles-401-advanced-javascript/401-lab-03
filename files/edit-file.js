e6dbf1207ecea32b87941633faba21065a14f0ee
travis skyles <travis.skyles@gmail.com>
2020-01-08 20:02:12 -0800

    added validator.js

--- files/edit-file.js
+++ files/edit-file.js
@@ -3,9 +3,20 @@
 const reader = require('./lib/reader');
 const file = `${__dirname}/data/person.json`;
 
+function Object(data){
+  this.firstName = data.firstName;
+  this.lastName = data.lastName;
+  this.hair = data.hair;
+  this.favoriteFoods = data.favoriteFoods;
+  this.married = data.married;
+  this.kids = data.kids;
+}
 
 reader.readerWithFilePromise(file)
-  .then(data => console.log('promise from reader: ', data))
+  .then(data => {
+    data.firstName = 'bob';
+    data.married = true;
+    console.log(data);
+  })
   .catch(err => {throw err;});
-
 
