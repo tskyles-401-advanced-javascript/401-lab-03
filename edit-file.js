af199c060530d342361860529376c2c9434f4773
travis skyles <travis.skyles@gmail.com>
2020-01-08 21:51:14 -0800

    basic writeFile complete

--- edit-file.js
+++ edit-file.js
@@ -3,9 +3,55 @@
 const file = `${__dirname}/files/data/person.json`;
 const util = require('util');
 const fs = require('fs');
+const validator = require('./files/lib/validator');
+
 
 let readFilePromise = util.promisify(fs.readFile);
 
-readFilePromise(file)
-  .then(data =>  console.log(JSON.parse(data)))
-  .catch(err => { throw err;});
+/**
+ * async function that get data from person.json file
+ * @param {*} file 
+ */
+async function readFile(file){
+  try {
+    let data = await readFilePromise(file);
+    let objectData = await JSON.parse(data);
+    return objectData;
+  }
+  catch {
+    throw error;
+  }
+}
+
+async function writeFile(){
+
+  const schema = {
+    firstName: { type: 'string', required: true },
+    lastName: { type: 'string', required: true },
+    hair: { type: 'object', required: true },
+    favoriteFoods: { type: 'array', required: true },
+    married: { type: 'boolean', required: true },
+    kids: { type: 'number', required: true },
+  }
+
+  try{
+    let data = await readFile(file);
+    data.firstName = 'Travis';
+    data.hair = { type: 'straight', color: 'brown' };
+    data.lastName = 'Skyles';
+    data.favoriteFoods = ['tacos', 'ice cream'];
+
+    if(validator.isValid(schema, data)){
+      console.log(data);
+
+    }
+  }
+  catch{
+    throw error;
+  }
+};
+
+writeFile();
+
+
+
