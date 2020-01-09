9f79c228e0a967d6d4c535a7d087427e0ae04fcc
travis skyles <travis.skyles@gmail.com>
2020-01-08 21:33:31 -0800

    created readFilePromis and async readFile

--- edit-file.js
+++ edit-file.js
@@ -4,3 +4,8 @@ const file = `${__dirname}/files/data/person.json`;
 const util = require('util');
 const fs = require('fs');
 
+let readFilePromise = util.promisify(fs.readFile);
+
+readFilePromise(file)
+  .then(data =>  console.log(JSON.parse(data)))
+  .catch(err => { throw err;});
